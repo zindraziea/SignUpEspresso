fe46dfabe8bb7962413fb9250213b7702b51aa71
KANITTHA HARNRATTANAPRASERT <kanittha.harnrattanaprasert@scb.co.th>
2019-07-15 21:33:32 +0700

    add first test

--- app/src/androidTest/java/com/sourcey/materiallogindemo/MainActivityTest.kt
+++ app/src/androidTest/java/com/sourcey/materiallogindemo/MainActivityTest.kt
@@ -1,5 +1,25 @@
 package com.sourcey.materiallogindemo
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun mainActivityTest() {
+        onView(withId(R.id.link_signup)).perform(click())
+    }
 }
